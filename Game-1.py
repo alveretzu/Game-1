b750f353a4dbfab23ed7d1c16f622da2f49c4563
alveretzu <luis.alveretzu@hotmail.com>
2014-11-01 23:13:11 -0700

    Luiss
    sd

--- Game-1.py
+++ Game-1.py
@@ -33,13 +33,8 @@ print ("""
 """)
 nombre= input("Como te llamas?")
 while (True):
-<<<<<<< HEAD
-	print "Estas en :",ciudad
-	lugar=input("\nDeseas : 1-Ir a la tienda 2-Ir a una casa 3-Ir al hospital 4-Ir al restaurante \n5-Ir al bosque 6-Ver el inventario 7-Ir al Bar")
-=======
 	print ("Estas en :",ciudad)
-	lugar=int(input("\nDeseas : 1-Ir a la tienda 2-Ir a una casa 3-Ir al hospital 4-Ir al restaurante \n5-Ir al bosque 6-Ver el inventario "))
->>>>>>> origin/master
+	lugar=int(input("\nDeseas : 1-Ir a la tienda 2-Ir a una casa 3-Ir al hospital 4-Ir al restaurante \n5-Ir al bosque 6-Ver el inventario 7-Ir al Bar"))
 	os.system('cls')
 	while (lugar==1):
 
